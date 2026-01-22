fa7e881460b995508ccb8414e847c0c77195b2e1
Rafi Zhafran <rafzha5@gmail.com>
2026-01-23 04:45:17 +0700

    Fix TS6133 unused Menu import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ShoppingCart, Menu, Search, Leaf, LogOut, User as UserIcon } from 'lucide-react';
+import { ShoppingCart, Search, Leaf, LogOut, User as UserIcon } from 'lucide-react';
 
 interface HeaderProps {
   cartCount: number;
